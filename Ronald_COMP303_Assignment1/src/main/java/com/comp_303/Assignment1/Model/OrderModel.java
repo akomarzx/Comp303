ff08f78f495e5b00d35ba042b04a6e89b1b18c6e
Ronald Jr Ombao <rombao@my.centennialcollege.ca>
2024-10-12 07:56:30 -0400

    Assignment 1 - Complete

--- Ronald_COMP303_Assignment1/src/main/java/com/comp_303/Assignment1/Model/OrderModel.java
+++ Ronald_COMP303_Assignment1/src/main/java/com/comp_303/Assignment1/Model/OrderModel.java
@@ -1,136 +1,173 @@
 package com.comp_303.Assignment1.Model;
 
-
 public class OrderModel {
-	
-	private String firstName;
-	private String middleName;
-	private String lastName;
-	private String emailAddress;
-	private String phoneAreaCode;
-	private String phoneNumber;
-	private String streetAddressLine1;
-	private String streetAddressLine2;
-	private String city;
-	private String province;
-	private String postalCode;
-	private String country;
-	
-	public OrderModel() {
-		super();
-	}
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String emailAddress;
+    private String phoneAreaCode;
+    private String phoneNumber;
+    private String streetAddressLine1;
+    private String streetAddressLine2;
+    private String city;
+    private String province;
+    private String postalCode;
+    private String country;
+    private String productNumber;
+    private String phoneBrand;
+    private String phoneModel;
+    private int quantity;
+    private double phonePrice;
+    private double totalPrice;
 
-	public OrderModel(String firstName, String middleName, String lastName, String emailAddress, String phoneAreaCode,
-			String phoneNumber, String streetAddressLine1, String streetAddressLine2, String city, String province,
-			String postalCode, String country) {
-		super();
-		this.firstName = firstName;
-		this.middleName = middleName;
-		this.lastName = lastName;
-		this.emailAddress = emailAddress;
-		this.phoneAreaCode = phoneAreaCode;
-		this.phoneNumber = phoneNumber;
-		this.streetAddressLine1 = streetAddressLine1;
-		this.streetAddressLine2 = streetAddressLine2;
-		this.city = city;
-		this.province = province;
-		this.postalCode = postalCode;
-		this.country = country;
-	}
+    // Getters and Setters...
 
-	public String getFirstName() {
-		return firstName;
-	}
+    public String getFirstName() {
+        return firstName;
+    }
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
-	public String getMiddleName() {
-		return middleName;
-	}
+    public String getMiddleName() {
+        return middleName;
+    }
 
-	public void setMiddleName(String middleName) {
-		this.middleName = middleName;
-	}
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
 
-	public String getLastName() {
-		return lastName;
-	}
+    public String getLastName() {
+        return lastName;
+    }
 
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
-	public String getEmailAddress() {
-		return emailAddress;
-	}
+    public String getEmailAddress() {
+        return emailAddress;
+    }
 
-	public void setEmailAddress(String emailAddress) {
-		this.emailAddress = emailAddress;
-	}
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
 
-	public String getPhoneAreaCode() {
-		return phoneAreaCode;
-	}
+    public String getPhoneAreaCode() {
+        return phoneAreaCode;
+    }
 
-	public void setPhoneAreaCode(String phoneAreaCode) {
-		this.phoneAreaCode = phoneAreaCode;
-	}
+    public void setPhoneAreaCode(String phoneAreaCode) {
+        this.phoneAreaCode = phoneAreaCode;
+    }
 
-	public String getPhoneNumber() {
-		return phoneNumber;
-	}
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
 
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 
-	public String getStreetAddressLine1() {
-		return streetAddressLine1;
-	}
+    public String getStreetAddressLine1() {
+        return streetAddressLine1;
+    }
 
-	public void setStreetAddressLine1(String streetAddressLine1) {
-		this.streetAddressLine1 = streetAddressLine1;
-	}
+    public void setStreetAddressLine1(String streetAddressLine1) {
+        this.streetAddressLine1 = streetAddressLine1;
+    }
 
-	public String getStreetAddressLine2() {
-		return streetAddressLine2;
-	}
+    public String getStreetAddressLine2() {
+        return streetAddressLine2;
+    }
 
-	public void setStreetAddressLine2(String streetAddressLine2) {
-		this.streetAddressLine2 = streetAddressLine2;
-	}
+    public void setStreetAddressLine2(String streetAddressLine2) {
+        this.streetAddressLine2 = streetAddressLine2;
+    }
 
-	public String getCity() {
-		return city;
-	}
+    public String getCity() {
+        return city;
+    }
 
-	public void setCity(String city) {
-		this.city = city;
-	}
+    public void setCity(String city) {
+        this.city = city;
+    }
 
-	public String getProvince() {
-		return province;
-	}
+    public String getProvince() {
+        return province;
+    }
 
-	public void setProvince(String province) {
-		this.province = province;
-	}
+    public void setProvince(String province) {
+        this.province = province;
+    }
 
-	public String getPostalCode() {
-		return postalCode;
-	}
+    public String getPostalCode() {
+        return postalCode;
+    }
 
-	public void setPostalCode(String postalCode) {
-		this.postalCode = postalCode;
-	}
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
 
-	public String getCountry() {
-		return country;
-	}
+    public String getCountry() {
+        return country;
+    }
 
-	public void setCountry(String country) {
-		this.country = country;
-	}
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getProductNumber() {
+        return productNumber;
+    }
+
+    public void setProductNumber(String productNumber) {
+        this.productNumber = productNumber;
+    }
+
+    public String getPhoneBrand() {
+        return phoneBrand;
+    }
+
+    public void setPhoneBrand(String phoneBrand) {
+        this.phoneBrand = phoneBrand;
+    }
+
+    public String getPhoneModel() {
+        return phoneModel;
+    }
+
+    public void setPhoneModel(String phoneModel) {
+        this.phoneModel = phoneModel;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+        updateTotalPrice();
+    }
+
+    public double getPhonePrice() {
+        return phonePrice;
+    }
+
+    public void setPhonePrice(double phonePrice) {
+        this.phonePrice = phonePrice;
+        updateTotalPrice();
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    private void updateTotalPrice() {
+        this.totalPrice = this.phonePrice * this.quantity;
+    }
 }
+
+
+
